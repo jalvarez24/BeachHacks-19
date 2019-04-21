163a3cbb880f916f131154877e55ddf70fbaaa88
Himanshu Patel <himanshup@csu.fullerton.edu>
2019-04-21 04:09:20 -0700

    update urls

--- beachhacks/beachcleaning/api/urls.py
+++ beachhacks/beachcleaning/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BeachesView, LoginView, LogoutView, RegisterView
+from .views import BeachesView, PostView, LoginView, LogoutView, RegisterView
 
 urlpatterns = [
     path('beaches/', BeachesView.as_view()),
+    path('posts/', PostView.as_view())
     path('auth/login/', LoginView.as_view()),
     path('auth/logout/', LogoutView.as_view()),
     path('auth/register/', RegisterView.as_view())
