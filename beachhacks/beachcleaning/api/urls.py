e29a1c256b10ae87cffb6aa3b119a6bba3402b1b
Himanshu Patel <himanshup@csu.fullerton.edu>
2019-04-21 04:09:50 -0700

    update urls

--- beachhacks/beachcleaning/api/urls.py
+++ beachhacks/beachcleaning/api/urls.py
@@ -3,7 +3,7 @@ from .views import BeachesView, PostView, LoginView, LogoutView, RegisterView
 
 urlpatterns = [
     path('beaches/', BeachesView.as_view()),
-    path('posts/', PostView.as_view())
+    path('posts/', PostView.as_view()),
     path('auth/login/', LoginView.as_view()),
     path('auth/logout/', LogoutView.as_view()),
     path('auth/register/', RegisterView.as_view())
